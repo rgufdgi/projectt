040253dff7b2041d88452bb217ee691cef856685
rgifdgi <rgufdgi@gmail.com>
2022-10-20 20:40:18 +0400

    Update data sort.py

--- data sort.py
+++ data sort.py
@@ -1,4 +1,46 @@
 # task 2
+
+def date_and_time(JD):  # перевод юлианской даты в григорианскую, UT
+    JDN = int(JD)
+    a = JDN + 32044
+    b = (4*a + 3) // 146097
+    c = a - (146097 * b) // 4
+    d = (4*c + 3) // 1461
+    e = c - 1461*d //4
+    m = (5 * e + 2)//153
+    day = e + 1 - (153 * m + 2) // 5
+    month = m + 3 - 12 * (m // 10)
+    year = 100*b + d - 4800 + m // 10
+    Jtime = JD - JDN
+    hour = int(Jtime * 24)
+    minute = int((Jtime*24 - hour) * 60)
+    second = round(((Jtime*24 - hour) * 60 - minute) * 60)
+    hour +=12
+    if hour > 24:
+        hour -= 24
+        day += 1
+    if len(str(hour)) == 1:
+        hour = f'0{str(hour)}'
+    if len(str(minute)) == 1:
+        minute = f'0{str(minute)}' 
+    if len(str(second)) == 1:
+        second = f'0{str(second)}'
+    if len(str(day)) == 1:
+        day = f'0{str(day)}'   
+    if len(str(month)) == 1:
+        month = f'0{str(month)}'        
+    date_and_time_str = f'{day}.{month}.{year} {hour}:{minute}:{second}'
+    return date_and_time_str
+
+# создаёт список с доступными для данного объекта фильтрами
+def available_filters(name, data_list): 
+    filter_list = []
+    for i in range(1, len(data_list)):
+        if data_list[i][0] == name and data_list[i][2] not in filter_list:
+            filter_list.append(data_list[i][2])
+    return filter_list
+
+
 data_file = open('task2_data.dat')
 all_strs = data_file.read().splitlines()  # создание списка строк
 data_file.close()
@@ -52,12 +94,6 @@ for i in range(len(names_list)):
     print(names_list[i]) 
 print()
 
-def available_filters(name, data_list):
-    filter_list = []
-    for i in range(1, len(data_list)):
-        if data_list[i][0] == name and data_list[i][2] not in filter_list:
-            filter_list.append(data_list[i][2])
-    return filter_list
 
 # вывод фильтров
 for i in range(len(names_list)):
@@ -66,7 +102,7 @@ for i in range(len(names_list)):
     print(*current_filters_list)
     print()
 
-# сортировка данных по юлианским датам 
+# сортировка данных по юлианским датам в порядке возрастания
 JD_list = []
 for i in range(1, len(all_strs)):
     JD_list.append(all_strs[i][1])
@@ -88,7 +124,7 @@ for i in range(1, len(JD_list)):
 # список для конкретного объекта
 star_name = input('Введите имя объекта: ')
 # проверка правильности вводимых данных
-if star_name not in names_list:
+while star_name not in names_list:
     print('Нет данных для этого объекта')
     star_name = input('Введите имя объекта: ')
 
@@ -101,61 +137,21 @@ for i in range(1, len(sorted_data_list)):
 all_filters = available_filters(star_name, star_list)
 filters = input('Введите названия фильтров, через пробел: ').split()
 # проверка правильности вводимых данных
-if len(filters) > 1:
-    for i in range(len(filters)):
-        if filters[i] not in all_filters:
-            print('Нет данных для этих фильтров')
-            print('Доступные фильтры: ')
-            print(*all_filters)
-            filters = input('Введите названия фильтров, через пробел: ').split()
-else:
-    if filters[0] not in all_filters:
-        print('Нет данных для этих фильтров')
-        print('Доступные фильтры: ')
-        print(*all_filters)
-        filters = input('Введите названия фильтров, через пробел: ').split()
+while set(filters).issubset(all_filters) == False:
+    print('Нет данных для этих фильтров')
+    print('Доступные фильтры:')
+    print(*all_filters)
+    filters = input('Введите названия фильтров, через пробел: ').split()
         
 almost_final_list = []  # список для данной звезды с данными фильтрами
 for i in range(len(star_list)):
         if star_list[i][2] in filters:
             almost_final_list.append(star_list[i])
             
-def Date_and_time(JD):  # перевод юлианской даты в григорианскую, UT
-    JDN = int(JD)
-    a = JDN + 32044
-    b = (4*a + 3) // 146097
-    c = a - (146097 * b) // 4
-    d = (4*c + 3) // 1461
-    e = c - 1461*d //4
-    m = (5 * e + 2)//153
-    day = e + 1 - (153 * m + 2) // 5
-    month = m + 3 - 12 * (m // 10)
-    year = 100*b + d - 4800 + m // 10
-    Jtime = JD - JDN
-    hour = int(Jtime * 24)
-    minute = int((Jtime*24 - hour) * 60)
-    second = round(((Jtime*24 - hour) * 60 - minute) * 60)
-    hour +=12
-    if hour > 24:
-        hour -= 24
-        day += 1
-    if len(str(hour)) == 1:
-        hour = f'0{str(hour)}'
-    if len(str(minute)) == 1:
-        minute = f'0{str(minute)}' 
-    if len(str(second)) == 1:
-        second = f'0{str(second)}'
-    if len(str(day)) == 1:
-        day = f'0{str(day)}'   
-    if len(str(month)) == 1:
-        month = f'0{str(month)}'        
-    date_and_time = f'{day}.{month}.{year} {hour}:{minute}:{second}'
-    return date_and_time
-
+# список дат чч.мм.гг и время
 usual_date = []
-
 for i in range(len(almost_final_list)):
-    usual_date_elem = Date_and_time(almost_final_list[i][1])
+    usual_date_elem = date_and_time(almost_final_list[i][1])
     usual_date.append(usual_date_elem)
     
 # преобразование юлианского дня к красивому виду    
@@ -164,15 +160,28 @@ for i in range(len(almost_final_list)):
     if num != 13:
         almost_final_list[i][1] = str(almost_final_list[i][1]) + '0' * (13 - num)
     almost_final_list[i][1] = str(almost_final_list[i][1])[2:] 
-    
-usual_date.insert(0, 'Date')
-almost_final_list.insert(0, [0, 'JD, 24...', 'Filter', 'Magnitude'])
-    
-final_file = open(f'{star_name}.data', 'w')
-#final_file.write('Date \t JD, 24.. \t Magnitude \t Filter\n')
+   
+
+# добавление даты в обычном формате в общий список
 for i in range(len(almost_final_list)):
-    final_file.write(f'{usual_date[i]}\t{almost_final_list[i][1]}\t\
- {almost_final_list[i][3]}\t{almost_final_list[i][2]}\n')
+    almost_final_list[i].insert(0, usual_date[i])
+    
+# сортировка по фильтрам
+num_of_filters = len(filters)
+final_list = []
+for i in range(num_of_filters):
+    current_filter = []
+    for j in range(1, len(almost_final_list)):
+        if almost_final_list[j][3] == filters[i]:
+            current_filter.append(almost_final_list[j])
+    final_list.extend(current_filter)
+
+
+# запись данных в файл   
+final_file = open(f'{star_name}.data', 'w')
+final_file.write('Date\t\t\t\t\tJD, 24.. \t Magnitude \t Filter\n')
+for i in range(len(final_list)):
+    final_file.write(f'{final_list[i][0]}\t\t{final_list[i][2]}\t\
+    {final_list[i][4]}\t\t{final_list[i][3]}\n')
 final_file.close()
         
-#исправить табуляцию
